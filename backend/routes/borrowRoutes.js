11866083377477f35f27e28c9d58db367bf4f76f
vikrant chauhan <i.vikrant.me@gmail.com>
2025-07-01 20:24:04 +0530

    borrow book update

--- backend/routes/borrowRoutes.js
+++ backend/routes/borrowRoutes.js
@@ -14,9 +14,9 @@ import {
 
 const router = express.Router();
 
-router.post("/record-borrowed-book/:id", isAuthenticated, isAuthorized("admin"),recordBorrowedBook);
+router.post("/record-borrowed-book/:id", isAuthenticated, isAuthorized("user"),recordBorrowedBook);
 router.get("/borrowed-books-by-all-users", isAuthenticated, isAuthorized("admin"),getBorrowedBooksForAdmin);
 router.get("/my-borrowed-books", isAuthenticated, borrowedBooks);
-router.put("/return-borrowed-book/:bookId",isAuthenticated,isAuthorized("admin"),returnBorrowedBooks);  
+router.put("/return-borrowed-book/:bookId",isAuthenticated,isAuthorized("user"),returnBorrowedBooks);  
 
 export default router;
